0beffad1a815a1366f1e9ec156e9e664db30589c
marv <marvelmd@gmail.com>
2018-11-07 23:03:00 +0300

    TaskCh02N013 done

---
+++ src/main/java/jdmarathon/tkhorik/TaskCh02N013.java
@@ -0,0 +1,34 @@
+package jdmarathon.tkhorik;
+
+/*Дано трехзначное число(100 ≤ X ≤ 999). Найти число, полученное при прочтении его цифр справа налево.
+Не использовать java.lang.String, java.lang.StringBuilder и т.п.
+Только числа и арифметические операции.
+Пример: 234 -> 432, 560 -> 65*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskCh02N013 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String response = "Yes";
+        do {
+            System.out.println("\nВведите трехачное целое число! Либо букву для прекращения программы");
+            try {
+                int temp = Integer.parseInt(scan.nextLine());
+                ArrayList<Integer> array = new ArrayList<>();
+                do {
+                    array.add(temp % 10);
+                    temp /= 10;
+                } while (temp > 0);
+
+                System.out.println(array);
+                int sum = array.stream().mapToInt(i -> i).sum();
+                System.out.print("Сумма цифр в числе:" + sum);
+            } catch (NumberFormatException nfe) {
+                System.err.println("Введено не число, программа прекращена!");
+                response = "No";
+            }
+        } while (response.equals("Yes"));
+    }
+}
